a6aab344cc7ad69d81eddfdfa306762cc66db1ec
PSD <mailbox.psd12@gmail.com>
2026-05-26 16:23:57 +0530

    Update key.sh

--- .config/bspwm/modules/key.sh
+++ .config/bspwm/modules/key.sh
@@ -10,6 +10,6 @@ fi
 export LC_ALL=en_US.UTF-8
 
 cat "$FILE" | sed 's/  */ /g' | column -t -s '|' -o '|' | \
-rofi -dmenu -i -theme ~/.config/rofi/themes/Cosmos.rasi -p " " \
+rofi -dmenu -i -p " " \
 -theme-str 'window { width: 900px; } listview { lines: 15;columns: 2; fixed-height: false; } element { padding: 2px 5px; } element-text { font: "JetBrainsMono Nerd Font 9";text-color: #ffffff; }' 
 
